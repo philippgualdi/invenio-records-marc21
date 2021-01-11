debd3280e831eb0b30a4daf4bb088d675f54ae0b
Philipp Gualdi <philipp.gualdi@tugraz.at>
2021-01-11 16:12:03 +0100

    modification: mappings move
    moving mapping to marc21 instead of records to prevent errors and
     removing unwanted contributors section

--- setup.py
+++ setup.py
@@ -86,6 +86,9 @@ setup(
         "invenio_i18n.translations": [
             "messages = invenio_records_marc21",
         ],
+        "invenio_search.mappings": [
+            "marc21 = invenio_records_marc21.mappings",
+        ],
         "invenio_pidstore.fetchers": [
             "marcid = invenio_records_marc21.fetchers:marc_pid_fetcher",
         ],
