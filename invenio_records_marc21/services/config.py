c178b2a3c2a4465373564d73a8cd98cadbf81566
Philipp Gualdi <philipp.gualdi@tugraz.at>
2023-08-16 11:35:29 +0000

    modification: records service config
    adding permission policy load from config and
    add  attributes for compatibility

--- invenio_records_marc21/services/config.py
+++ invenio_records_marc21/services/config.py
@@ -12,6 +12,7 @@
 
 
 from invenio_drafts_resources.services.records.components import DraftFilesComponent
+from invenio_records_resources.services.base.config import FromConfig
 from invenio_drafts_resources.services.records.config import (
     RecordServiceConfig,
     SearchDraftsOptions,
